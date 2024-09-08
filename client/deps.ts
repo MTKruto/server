9fb7de56f19aa0118f4f65e72483e564582fd8ee
Roj <ez@roj.im>
2024-09-08 19:57:53 +0300

    Update to MTKruto 0.2.41

--- client/deps.ts
+++ client/deps.ts
@@ -22,9 +22,9 @@ export { unreachable } from "https://deno.land/std@0.224.0/assert/unreachable.ts
 export { unimplemented } from "https://deno.land/std@0.224.0/assert/unimplemented.ts";
 
 export type { BotCommand, BusinessConnection, CallbackQueryAnswer, Chat, ChatMember, Client as Client_, Context as Context_, FailedInvitation, InactiveChat, InlineQueryAnswer, InviteLink, LiveStreamChannel, Message, MessageAnimation, MessageAudio, MessageContact, MessageDice, MessageDocument, MessageInvoice, MessageLocation, MessagePhoto, MessagePoll, MessageSticker, MessageText, MessageVenue, MessageVideo, MessageVideoNote, MessageVoice, Poll, ReplyTo, Sticker, Story, Update, User, VideoChat, VideoChatActive, VideoChatScheduled } from "https://deno.land/x/mtkruto@0.2.30/mod.ts";
-export { cleanObject } from "https://deno.land/x/mtkruto@0.2.30/utilities/0_object.ts";
-export { iterateReadableStream } from "https://deno.land/x/mtkruto@0.2.30/utilities/1_misc.ts";
-export { Composer } from "https://deno.land/x/mtkruto@0.2.30/client/1_composer.ts";
-export { resolve } from "https://deno.land/x/mtkruto@0.2.30/client/0_utilities.ts";
+export { cleanObject } from "https://deno.land/x/mtkruto@0.2.41/utilities/0_object.ts";
+export { iterateReadableStream } from "https://deno.land/x/mtkruto@0.2.41/utilities/1_misc.ts";
+export { Composer } from "https://deno.land/x/mtkruto@0.2.41/client/1_composer.ts";
+export { resolve } from "https://deno.land/x/mtkruto@0.2.41/client/0_utilities.ts";
 export type { AllowedMethod } from "../allowed_methods.ts";
 export { transform } from "../transform.ts";
